6cf9da115d8d04f0d6d8d8f28b5999cf40beb372
ayrbox <srdx7@hotmail.com>
2020-04-14 23:32:49 +0100

    Create random string function in lib

---
+++ pizza-delivery/lib/makeRandomString.js
@@ -0,0 +1,19 @@
+const makeRandomString = function(length) {
+  if(!(typeof(length) === 'number' && length > 0 && length)) {
+    throw new Error('Invalid length to generate random string.');
+  }
+
+  return function() {
+    const size = typeof(length) === 'number' && length > 0 && length;
+    if (!size) {
+      return false;
+    }
+
+    const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    return new Array(size).fill(
+      () => CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length))
+    ).map(f => f()).join('');
+  }
+}
+
+module.exports = makeRandomString;
