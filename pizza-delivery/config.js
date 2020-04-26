d845af05341fe3884f6b051f658b622c6b1ac75f
ayrbox <srdx7@hotmail.com>
2020-04-27 00:57:08 +0100

    Update config with require mailgun and stripe keys

--- pizza-delivery/config.js
+++ pizza-delivery/config.js
@@ -1,6 +1,17 @@
 const config = {
-  hashingSecret: 'NxwxZBTu4ntWTRY42utvpftv@geff{Fh'
-}
-
+  hashingSecret: "NxwxZBTu4ntWTRY42utvpftv@geff{Fh",
+  stripe: {
+    url: "api.stripe.com",
+    key: "<STRIP-APIKEY>",
+    currency: "gbp",
+    paymentMethods: "card"
+  },
+  mailgun: {
+    apiKey: "<MAILGUN-APIKEY>",
+    host: "api.mailgun.net",
+    domain: "<MAILGUN-DOMAIN-SETUP>",
+    from: "Pizza Delivery <pizzy-delivery@pirple.com>"
+  }
+};
 
 module.exports = config;
