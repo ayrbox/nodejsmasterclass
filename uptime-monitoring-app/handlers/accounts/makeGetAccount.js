40f3cefafb5dd5a78abf4afad5f13dc2b7787a94
ayrbox <srdx7@hotmail.com>
2020-07-07 23:55:36 +0100

    Render with layout.

--- uptime-monitoring-app/handlers/accounts/makeGetAccount.js
+++ uptime-monitoring-app/handlers/accounts/makeGetAccount.js
@@ -2,7 +2,7 @@ module.exports = function (render) {
   return function (data, callback) {
     render('index.html', function (err, content) {
       if (err) {
-        callback(500, error);
+        callback(500, err);
         return;
       }
       callback(200, content, 'text/html');
