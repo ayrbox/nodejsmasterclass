47ee0ae8a6ed22bef0477fdb77bd5f6d54ecd78c
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2020-07-16 00:21:16 +0100

    Feature to add layout engine.

--- uptime-monitoring-app/handlers/accounts/makeGetAccount.js
+++ uptime-monitoring-app/handlers/accounts/makeGetAccount.js
@@ -1,11 +1,15 @@
-module.exports = function (render) {
-  return function (data, callback) {
-    render('index.html', function (err, content) {
-      if (err) {
-        callback(500, err);
-        return;
-      }
-      callback(200, content, 'text/html');
-    });
+module.exports = function(render) {
+  return function(data, callback) {
+    render(
+      "index.html",
+      function(err, content) {
+        if (err) {
+          callback(500, err);
+          return;
+        }
+        callback(200, content, "text/html");
+      },
+      "_layout.html"
+    );
   };
 };
--- uptime-monitoring-app/lib/renderer.js
+++ uptime-monitoring-app/lib/renderer.js
@@ -1,18 +1,45 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
+
+function templateEngine(templateString, data) {
+  const regEx = /<%([^%>]+)?%>/g;
+  let match;
+  let tpl = templateString;
+  while ((match = regEx.exec(templateString))) {
+    const [key, value] = match;
+
+    tpl = tpl.replace(key, data[value]);
+  }
+  return tpl;
+}
 
 function makeRender(viewFolder) {
-  return function (viewFile, callback, layoutView = undefined) {
+  return function(viewFile, callback, layoutView = undefined) {
     // TODO: if layout view provided wrap the content withint layoutView content
-    var filePath = path.join(viewFolder, viewFile);
+    const filePath = path.join(viewFolder, viewFile);
 
-    fs.readFile(filePath, 'utf8', function (err, content) {
+    fs.readFile(filePath, "utf8", function(err, content) {
       if (err) {
-        callback(new Error('Unable render template.'));
+        callback(new Error("Unable render template."));
         return;
       }
 
-      callback(false, content);
+      if (!layoutView) {
+        callback(false, content);
+        return;
+      }
+
+      const layout = path.join(viewFolder, layoutView);
+      fs.readFile(layout, "utf8", function(err, layoutContent) {
+        if (err) {
+          callback(new Error("Layout not found."));
+          return;
+        }
+
+        const data = { content };
+        const resultHtml = templateEngine(layoutContent, data);
+        callback(false, resultHtml);
+      });
     });
   };
 }
