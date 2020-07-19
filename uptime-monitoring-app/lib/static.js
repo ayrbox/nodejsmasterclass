8bf7a937d2f2b0c07a2481f511c7231537c06fd6
ayrbox <srdx7@hotmail.com>
2020-07-19 21:57:21 +0100

    Add mime type for files.

--- uptime-monitoring-app/lib/static.js
+++ uptime-monitoring-app/lib/static.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const fs = require('fs');
 
+const mimeTypes = {
+  txt: 'text/plain',
+  css: 'text/css',
+  js: 'text/javascript',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpg',
+  png: 'image/png',
+  html: 'text/html',
+  favicon: 'image/x-icon', // TODO
+};
+
 function makeStatic(staticFolder) {
   return function (reqPath, callback) {
     const filePath = path.join(staticFolder, reqPath);
+
+    const ext = filePath.match(/\w+$/i)[0];
 
     fs.readFile(filePath, 'utf-8', function (err, content) {
       if (err) {
         callback(new Error('Error reading file'));
         return;
       }
-      callback(false, content, 'text/plain');
+      callback(false, content, mimeTypes[ext] || 'text/plain');
     });
   };
 }
