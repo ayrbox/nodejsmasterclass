6e752f77a21bc268bcdc31354373fb03047226d4
ayrbox <srdx7@hotmail.com>
2020-07-19 22:02:31 +0100

    WIP xhr/ajax client for browser.

--- uptime-monitoring-app/public/app.js
+++ uptime-monitoring-app/public/app.js
@@ -1,1 +1,21 @@
 console.log('Hello Console');
+
+var app = {};
+
+app.config = {
+  sessionToken: false,
+};
+
+// Ajax cleint
+//
+//
+app.client = {};
+
+app.client.request = function (
+  headers,
+  path,
+  method,
+  queryString,
+  payload,
+  callback
+) {};
