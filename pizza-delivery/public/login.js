346c51c6b40fa4ae625866b166775c81c4629fbf
ayrbox <srdx7@hotmail.com>
2020-08-02 23:48:15 +0100

    Create token on login form summit.

---
+++ pizza-delivery/public/login.js
@@ -0,0 +1,36 @@
+app.on('afterInit', function () {
+  var alerts = document.querySelectorAll('.alert');
+  for (var idx = 0; idx < alerts.length; idx++) {
+    alerts[idx].style.display = 'none';
+  }
+
+  app.bindForm('loginForm');
+});
+
+app.on('response', function (args) {
+  var responsePayload = args.responsePayload;
+
+  var error =
+    typeof responsePayload.message == 'string'
+      ? responsePayload.message
+      : 'API request is successful';
+
+  var formSuccess = document.querySelector(
+    '#' + args.formId + ' .alert-success'
+  );
+  formSuccess.innerHTML = error;
+  formSuccess.style.display = 'block';
+});
+
+app.on('response-error', function (args) {
+  var responsePayload = args.responsePayload;
+
+  var error =
+    typeof responsePayload.message == 'string'
+      ? responsePayload.message
+      : 'An error has occured, please try again';
+
+  var formError = document.querySelector('#' + args.formId + ' .alert-danger');
+  formError.innerHTML = error;
+  formError.style.display = 'block';
+});
