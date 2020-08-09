45e1e449d6ce7ce212c23b0108d70d2e0902cfc4
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2020-08-09 03:59:32 +0100

    List cart items.

--- pizza-delivery/public/checkout.js
+++ pizza-delivery/public/checkout.js
@@ -4,7 +4,30 @@ app.on("afterInit", function() {
 });
 
 app.on("cart-refresh", function() {
-  console.log(app.cart);
+  var listEl = document.querySelector("#list-items");
+  var itemsHtml = "";
+
+  for (var item of app.cart.items) {
+    itemsHtml += `
+    <li class="list-group-item d-flex justify-content-between lh-condensed" data-id="${
+      item.menuId
+    }">
+      <div>
+        <h6 class="my-0">${item.name}</h6
+        <small class="text-muted">Size: ${item.option.name} | Price: ${
+      item.option.price
+    } | Qty: ${item.quantity}</small>
+      </div>
+      <span class="text-muted">${item.amount.toFixed(2)}</span>
+    </li>`;
+  }
+  itemsHtml += `
+    <li class="list-group-item d-flex justify-content-between">
+      <span>Total</span>
+      <strong>${app.cart.total.toFixed(2)}</strong>
+    </li>`;
+
+  listEl.innerHTML = itemsHtml;
 });
 
 app.on("user-updated", function(user) {
