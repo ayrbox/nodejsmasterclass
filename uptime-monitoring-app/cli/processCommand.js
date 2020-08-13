af9540627827e98a56e590860c3fd3b5aa9926d6
ayrbox <srdx7@hotmail.com>
2020-08-13 01:34:34 +0100

    Read command string and extracts options.

--- uptime-monitoring-app/cli/processCommand.js
+++ uptime-monitoring-app/cli/processCommand.js
@@ -1,24 +1,58 @@
 const handlers = require('./handlers');
 
-const defaultHandler = function () {
-  console.log('Unknown command. Please try again.');
+const extractOptionValue = function ({ command, pattern }) {
+  const regEx = new RegExp(pattern);
+  const match = command.match(regEx, 'g');
+
+  if (match) {
+    const [, value] = match;
+    return value.match(/--[^\s]+/, 'g') ? true : value;
+  } else {
+    return true;
+  }
 };
 
-const processCommand = async function (command) {
+function extractCommandWithOptions(commandString) {
+  const [command] = commandString.split(/\s/);
+  const regEx = /--([^\s]+)\s?/g;
+
+  let match;
+  const options = {};
+  while ((match = regEx.exec(commandString))) {
+    const [, option] = match;
+
+    const value = extractOptionValue({
+      command: commandString,
+      pattern: `--${option}\\s([^\\s]+)\\s?`,
+    });
+    options[option] = value;
+  }
+  return [command, options];
+}
+
+const defaultHandler = function (command) {
+  console.log(`Unknown command "${command}". Please try again.`);
+};
+
+const processCommand = async function (rawCommand) {
   const commandString =
-    typeof command == 'string' && command.trim().length > 0
-      ? command.trim()
+    typeof rawCommand == 'string' && rawCommand.trim().length > 0
+      ? rawCommand.trim()
       : false;
 
-  if (commandString) {
-    const commandHandler = handlers.find(
-      handler => handler.command === command
-    ) || { handler: defaultHandler };
-
-    const commandArgs = command; // TODO: extract command args
-
-    commandHandler.handler(commandArgs);
+  if (!commandString) {
+    return;
   }
+
+  const [command, options] = extractCommandWithOptions(commandString);
+
+  const commandHandler = handlers.find(
+    handler => handler.command === command
+  ) || {
+    handler: defaultHandler,
+  };
+
+  commandHandler.handler(command, options);
 };
 
 module.exports = processCommand;
